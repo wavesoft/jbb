be996ee6bae62f5f8d1a12d83ae0e14871a19e67
Ioannis Charalampidis <icharala@macaliceichar.cern.ch>
2016-02-10 17:02:09 +0100

    Draft ideas for object flattening

---
+++ test/flattening/encode.js
@@ -0,0 +1,136 @@
+
+var struct = [{"name":".morphTargetInfluences[stand001]","keys":[{"time":0,"value":1,"constantToNext":false},{"time":0.10000000149011612,"value":0,"constantToNext":true},{"time":3.9000000953674316,"value":0},{"time":4,"value":1}],"lastIndex":0,"result":1},{"name":".morphTargetInfluences[stand002]","keys":[{"time":0,"value":0,"constantToNext":false},{"time":0.10000000149011612,"value":1,"constantToNext":false},{"time":0.20000000298023224,"value":0},{"time":4,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand003]","keys":[{"time":0.10000000149011612,"value":0,"constantToNext":false},{"time":0.20000000298023224,"value":1},{"time":0.30000001192092896,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand004]","keys":[{"time":0.20000000298023224,"value":0,"constantToNext":false},{"time":0.30000001192092896,"value":1},{"time":0.4000000059604645,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand005]","keys":[{"time":0.30000001192092896,"value":0,"constantToNext":false},{"time":0.4000000059604645,"value":1},{"time":0.5,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand006]","keys":[{"time":0.4000000059604645,"value":0,"constantToNext":false},{"time":0.5,"value":1},{"time":0.6000000238418579,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand007]","keys":[{"time":0.5,"value":0,"constantToNext":false},{"time":0.6000000238418579,"value":1},{"time":0.699999988079071,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand008]","keys":[{"time":0.6000000238418579,"value":0,"constantToNext":false},{"time":0.699999988079071,"value":1},{"time":0.800000011920929,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand009]","keys":[{"time":0.699999988079071,"value":0,"constantToNext":false},{"time":0.800000011920929,"value":1},{"time":0.8999999761581421,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand010]","keys":[{"time":0.800000011920929,"value":0,"constantToNext":false},{"time":0.8999999761581421,"value":1},{"time":1,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand011]","keys":[{"time":0.8999999761581421,"value":0,"constantToNext":false},{"time":1,"value":1},{"time":1.100000023841858,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand012]","keys":[{"time":1,"value":0,"constantToNext":false},{"time":1.100000023841858,"value":1},{"time":1.2000000476837158,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand013]","keys":[{"time":1.100000023841858,"value":0,"constantToNext":false},{"time":1.2000000476837158,"value":1},{"time":1.2999999523162842,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand014]","keys":[{"time":1.2000000476837158,"value":0,"constantToNext":false},{"time":1.2999999523162842,"value":1},{"time":1.399999976158142,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand015]","keys":[{"time":1.2999999523162842,"value":0,"constantToNext":false},{"time":1.399999976158142,"value":1},{"time":1.5,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand016]","keys":[{"time":1.399999976158142,"value":0,"constantToNext":false},{"time":1.5,"value":1},{"time":1.600000023841858,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand017]","keys":[{"time":1.5,"value":0,"constantToNext":false},{"time":1.600000023841858,"value":1},{"time":1.7000000476837158,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand018]","keys":[{"time":1.600000023841858,"value":0,"constantToNext":false},{"time":1.7000000476837158,"value":1},{"time":1.7999999523162842,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand019]","keys":[{"time":1.7000000476837158,"value":0,"constantToNext":false},{"time":1.7999999523162842,"value":1},{"time":1.899999976158142,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand020]","keys":[{"time":1.7999999523162842,"value":0,"constantToNext":false},{"time":1.899999976158142,"value":1},{"time":2,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand021]","keys":[{"time":1.899999976158142,"value":0,"constantToNext":false},{"time":2,"value":1},{"time":2.0999999046325684,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand022]","keys":[{"time":2,"value":0,"constantToNext":false},{"time":2.0999999046325684,"value":1},{"time":2.200000047683716,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand023]","keys":[{"time":2.0999999046325684,"value":0,"constantToNext":false},{"time":2.200000047683716,"value":1},{"time":2.299999952316284,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand024]","keys":[{"time":2.200000047683716,"value":0,"constantToNext":false},{"time":2.299999952316284,"value":1},{"time":2.4000000953674316,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand025]","keys":[{"time":2.299999952316284,"value":0,"constantToNext":false},{"time":2.4000000953674316,"value":1},{"time":2.5,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand026]","keys":[{"time":2.4000000953674316,"value":0,"constantToNext":false},{"time":2.5,"value":1},{"time":2.5999999046325684,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand027]","keys":[{"time":2.5,"value":0,"constantToNext":false},{"time":2.5999999046325684,"value":1},{"time":2.700000047683716,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand028]","keys":[{"time":2.5999999046325684,"value":0,"constantToNext":false},{"time":2.700000047683716,"value":1},{"time":2.799999952316284,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand029]","keys":[{"time":2.700000047683716,"value":0,"constantToNext":false},{"time":2.799999952316284,"value":1},{"time":2.9000000953674316,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand030]","keys":[{"time":2.799999952316284,"value":0,"constantToNext":false},{"time":2.9000000953674316,"value":1},{"time":3,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand031]","keys":[{"time":2.9000000953674316,"value":0,"constantToNext":false},{"time":3,"value":1},{"time":3.0999999046325684,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand032]","keys":[{"time":3,"value":0,"constantToNext":false},{"time":3.0999999046325684,"value":1},{"time":3.200000047683716,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand033]","keys":[{"time":3.0999999046325684,"value":0,"constantToNext":false},{"time":3.200000047683716,"value":1},{"time":3.299999952316284,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand034]","keys":[{"time":3.200000047683716,"value":0,"constantToNext":false},{"time":3.299999952316284,"value":1},{"time":3.4000000953674316,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand035]","keys":[{"time":3.299999952316284,"value":0,"constantToNext":false},{"time":3.4000000953674316,"value":1},{"time":3.5,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand036]","keys":[{"time":3.4000000953674316,"value":0,"constantToNext":false},{"time":3.5,"value":1},{"time":3.5999999046325684,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand037]","keys":[{"time":3.5,"value":0,"constantToNext":false},{"time":3.5999999046325684,"value":1},{"time":3.700000047683716,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand038]","keys":[{"time":3.5999999046325684,"value":0,"constantToNext":false},{"time":3.700000047683716,"value":1},{"time":3.799999952316284,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand039]","keys":[{"time":3.700000047683716,"value":0,"constantToNext":false},{"time":3.799999952316284,"value":1},{"time":3.9000000953674316,"value":0}],"lastIndex":0,"result":0},{"name":".morphTargetInfluences[stand040]","keys":[{"time":0,"value":0,"constantToNext":true},{"time":3.799999952316284,"value":0,"constantToNext":false},{"time":3.9000000953674316,"value":1},{"time":4,"value":0}],"lastIndex":0,"result":0}];
+
+
+var WALK_OP = {
+	BEGIN_OBJECT 		: 0x01,
+	BEGIN_VALUE_ARRAY	: 0x02,
+	BEGIN_OBJECT_ARRAY	: 0x03,
+};
+
+var WALK_OP = {
+	TYPE_VAL 	: 0x01, // ......01 = Value
+	TYPE_ARR 	: 0x02, // ......10 = Array
+	TYPE_DICT	: 0x03, // ......11 = Dictionary
+	WITH_KEY	: 0x04, // .....1.. = Place on Key [+1B]
+	LEN_8		: 0x00, // ..00.... = Length (8 Bit)
+	LEN_16		: 0x00, // ..01.... = Length (16 Bit)
+	LEN_32		: 0x00, // ..10.... = Length (32 Bit)
+	LEN_64		: 0x00, // ..11.... = Length (64 Bit) .. lol!
+};
+
+/*******************\
+
+
+var = 0.0;		 // TYPE_VAL
+var = [0.0,0.0]	 // TYPE_ARR + LEN_??
+var = {			 // TYPE_DICT
+	'k'	: 0.0,	 // WITH_KEY + TYPE_VAL
+	'v'	: [0,0], // WITH_KEY + TYPE_ARR + INDEX_??
+	'K': {		 // WITH_KEY + TYPE_DICT
+		...
+	},
+	'K': [		 // WITH_KEY + TYPE_ARR
+		{		 // 
+		}
+	]
+};
+
+
+\*******************/
+
+/**
+ * Walk structure and flatten
+ */
+function walk( data, s_steer, s_data, s_data_keys, key_id )
+{
+	var ans = [], len, id, keys, key, op = 0;
+	console.log(key_id,"->",data);
+
+	// Arrays
+	if ((data instanceof Uint8Array) || (data instanceof Int8Array) ||
+		(data instanceof Uint16Array) || (data instanceof Int16Array) ||
+		(data instanceof Uint32Array) || (data instanceof Int32Array) ||
+		(data instanceof Float32Array) || (data instanceof Float64Array) ||
+		(data instanceof Array)) {
+
+		// Get data length
+		len = data.length;
+		op = WALK_OP.LEN_8;
+		if (len < 65536) {
+			op = WALK_OP.LEN_16;
+		} else if (len < 4294967296) {
+			op = WALK_OP.LEN_32;
+		}
+
+		// Store metadata on stream
+		if (key_id !== undefined) {
+			s_steer.push( WALK_OP.TYPE_ARR | WALK_OP.WITH_KEY | op );
+			s_steer.push( key_id );
+		} else {
+			s_steer.push( WALK_OP.TYPE_ARR | op );
+		}
+
+		// Store length
+		s_steer.push( len );
+
+		// Encode array
+		for (var i=0, l=data.length; i<l; i++) {
+			s_data[key_id].push( data );
+		}
+
+	// Objects
+	} else if ( data.constructor === ({}).constructor ) {
+
+		// Store metadata on stream
+		if (key_id !== undefined) {
+			s_steer.push( WALK_OP.TYPE_DICT | WALK_OP.WITH_KEY );
+			s_steer.push( key_id );
+		} else {
+			s_steer.push( WALK_OP.TYPE_DICT);
+		}
+
+		// Encode keys
+		keys = Object.keys(data);
+		for (var i=0, l=keys.length; i<l; i++) {
+			key = keys[i];
+
+			// Get key ID
+			key_id = s_data_keys.indexOf(key);
+			if (key_id < 0) {
+				key_id = s_data_keys.length;
+				s_data.push([]);
+				s_data_keys.push(key);
+			}
+
+			// Encode
+			walk( data[key], s_steer, s_data, s_data_keys, key_id );
+
+		}
+
+	// Values
+	} else {
+
+		// Store metadata on stream
+		if (key_id !== undefined) {
+			s_steer.push( WALK_OP.TYPE_VAL | WALK_OP.WITH_KEY );
+			s_steer.push( key_id );
+		} else {
+			s_steer.push( WALK_OP.TYPE_VAL);
+		}
+
+		// Push value on stream
+		s_data[key_id].push( data );
+
+	}
+
+}
+
+
+var a = [], b = [[]], c = [undefined];
+walk( struct, a, b, c, 0 );
+
+console.log(a);
+console.log(b);
+console.log(c);
+
